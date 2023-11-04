caedb08c84ea4af4abcd9019e18fcef82d1472cf
Ala Arab <alaarab@gmail.com>
2023-11-03 17:54:21 -0700

    download from s3

--- download_from_s3.py
+++ download_from_s3.py
@@ -1,6 +1,6 @@
-from lib.aws import assume_role, get_s3_client
+import os
+from lib.aws import *
 from config import *
-
 
 def main():
     # Assume the role
@@ -9,8 +9,20 @@ def main():
     # Get the S3 client
     s3_client = get_s3_client(aws_credentials)
 
+    # List objects in the S3 bucket
+    s3_objects = s3_client.list_objects_v2(Bucket=S3_BUCKET)['Contents']
 
-
+    # Download each object
+    for obj in s3_objects:
+        key = obj['Key']
+        download_path = os.path.join(LOCAL_DOWNLOAD_PATH, key)
+        
+        # Ensure the directory structure exists before downloading
+        directory = os.path.dirname(download_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        download_from_s3(s3_client, S3_BUCKET, key, download_path)
 
 if __name__ == "__main__":
     main()
